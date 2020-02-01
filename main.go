acf97183a3213aae6bda61356ce8097856b8e989
Thomas McCall <thomas.e.mccall@gmail.com>
2020-02-01 14:47:54 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type logServer struct{}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
